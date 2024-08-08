13433400ee08d9315cbe12105aaae8d292197f6b
Jekko07 <jerichojamesranara@gmail.com>
2024-08-08 15:31:57 +0800

    added edit functionality completed crud functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,15 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
   //Add New Job
   const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
+    const res = await fetch("/api/jobs", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newJob),
     });
@@ -27,12 +28,22 @@ const App = () => {
   //Delete job
   const deleteJob = async (id) => {
     const res = await fetch(`/api/jobs/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
     return;
-  }
+  };
 
-
+  //Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -40,7 +51,16 @@ const App = () => {
         <Route index element={<Homepage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
